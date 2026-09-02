e93bb824db35ec1c15aeb65c471ddbc07ad16593
EmmyTunz <tummagold@gmail.com>
2026-09-02 06:58:50 +0100

    Fill google form with property data using selenium

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 import requests
+
 google_form_link = "https://docs.google.com/forms/d/e/1FAIpQLSeeBklGTpr3CI3RZUL7PRonCV6fHZZmTxebar8_LiUrpl3mgA/viewform?usp=publish-editor"
 
-
+# scrape demo Zillow website with beautifulsoup
 response = requests.get("https://appbrewery.github.io/Zillow-Clone/")
 website_html = response.text
 soup = BeautifulSoup(website_html, "html.parser")
@@ -18,7 +21,6 @@ for i in span_list:
     except KeyError:
         pass
 
-
 price_list = [i.split("+")[0].split("/")[0] for i in raw_price_list]
 
 # scrape address data
@@ -31,8 +33,32 @@ link_list = []
 for a in a_list:
     try:
         if a["data-test"] == "property-card-link":
-            link_list.append(a["href"])
+            if a["href"] in link_list:
+                pass
+            else:
+                link_list.append(a["href"])
     except KeyError:
         pass
+# link_list = list(set(link_list))
 
 print(link_list)
+
+
+
+# fill google form with property data with selenium
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=chrome_options)
+driver.get(google_form_link)
+
+input_fields = driver.find_elements(By.CSS_SELECTOR, value='input[type="text"]')
+submit_button = driver.find_element(By.XPATH, value="//*[@id='mG61Hd']/div[2]/div/div[3]/div[1]/div[1]/div")
+
+for i, j, k in zip(address_list, price_list, link_list):
+    input_fields[0].send_keys(i)
+    input_fields[1].send_keys(j)
+    input_fields[2].send_keys(k)
+
+    submit_button.click()
+
+
