4925fc8c114f71034dc9db042e4664dcdc656796
EmmyTunz <tummagold@gmail.com>
2026-09-02 08:26:28 +0100

    Fixed a bug where new responses where not created due to stale HTML elements

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import StaleElementReferenceException, NoSuchElementException
 import requests
+import time
 
 google_form_link = "https://docs.google.com/forms/d/e/1FAIpQLSeeBklGTpr3CI3RZUL7PRonCV6fHZZmTxebar8_LiUrpl3mgA/viewform?usp=publish-editor"
 
@@ -25,7 +27,7 @@ price_list = [i.split("+")[0].split("/")[0] for i in raw_price_list]
 
 # scrape address data
 address_list = [i.text.split("\n")[1].split("                                  ")[1] for i in soup.find_all("address")]
-print(address_list)
+
 
 # scrape property links
 a_list = soup.find_all("a")
@@ -39,9 +41,7 @@ for a in a_list:
                 link_list.append(a["href"])
     except KeyError:
         pass
-# link_list = list(set(link_list))
 
-print(link_list)
 
 
 
@@ -51,14 +51,25 @@ chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
 driver.get(google_form_link)
 
-input_fields = driver.find_elements(By.CSS_SELECTOR, value='input[type="text"]')
+
 submit_button = driver.find_element(By.XPATH, value="//*[@id='mG61Hd']/div[2]/div/div[3]/div[1]/div[1]/div")
 
 for i, j, k in zip(address_list, price_list, link_list):
-    input_fields[0].send_keys(i)
-    input_fields[1].send_keys(j)
-    input_fields[2].send_keys(k)
+    time.sleep(5)
+    input_fields = driver.find_elements(By.CSS_SELECTOR, value='input[type="text"]')
+    print(input_fields)
+    try:
+        input_fields[0].send_keys(i)
+        input_fields[1].send_keys(j)
+        input_fields[2].send_keys(k)
 
-    submit_button.click()
+        submit_button = driver.find_element(By.XPATH, value="//*[@id='mG61Hd']/div[2]/div/div[3]/div[1]/div[1]/div")
+        submit_button.click()
+    except StaleElementReferenceException, IndexError:
+        submit_new_response = driver.find_element(By.XPATH, value="/html/body/div[1]/div[2]/div[1]/div/div[4]/a")
+        submit_new_response.click()
+        time.sleep(5)
+
+
 
 
